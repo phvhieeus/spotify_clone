d8197506777426cbdd2c46d9327e00b0312283fd
phvhieeus <hieuvanpham1211@gmail.com>
2025-03-12 15:52:27 +0700

    update ytn api 1

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -56,21 +56,6 @@ const Player = () => {
             {track?.desc || "Artist"}
           </p>
 
-          {/* Add YouTube indicator */}
-          {usingYouTube && (
-            <p className="text-[#1DB954] text-xs flex items-center mt-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-3 w-3 mr-1"
-                fill="currentColor"
-                viewBox="0 0 576 512"
-              >
-                <path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64s-170.8 0-213.4 11.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z" />
-              </svg>
-              Đang phát từ YouTube
-            </p>
-          )}
-
           {/* Display error message */}
           {error && <p className="text-red-500 text-xs mt-1">{error}</p>}
         </div>
